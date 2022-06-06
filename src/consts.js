a6ddba1270c99b70739917d63340ba58ba8d17bd
2PAYC <apelordfayc@gmail.com>
2022-06-06 11:26:09 -0700

    clean up contract code + update prod URI

--- src/consts.js
+++ src/consts.js
@@ -3,7 +3,7 @@ export const paycMarketPlaceContractAddr =
   '0xd2d69f530dd343eaa0bbc03ddd099cef0855da52'
 export const payc721ContractAddr = '0xC3C412164822C42F94D14F61620921d22ecbc621'
 export const paycSubGraphAPI =
-  'https://api.studio.thegraph.com/query/18229/paycmarket/v2.5.4'
+  'https://api.studio.thegraph.com/query/18229/paycmarket/v2.5.5'
 export const localDirectory = 'https://payc-images.s3.amazonaws.com/ipfs/'
 export const web3ProviderURL =
   'https://rinkeby.infura.io/v3/1b374167f9f44a9aab01f75e8367a34a'
